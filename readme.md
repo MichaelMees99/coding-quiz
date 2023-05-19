59453c57889fdf32af2b79c8a8edf93113aa5bce
MichaelMees99 <120432979+MichaelMees99@users.noreply.github.com>
2023-05-18 23:38:05 -0400

    Final changes v1.0
    added minor graphical tweaks
    added the functionality to see high score from main page
    added random questions
    added new readme file

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Mike's Coding Quiz
+
+![Image of the main page of coding quiz](/images/Screenshot%202023-05-18%20233052.png)
+
+Have fun with this short and simple application featuring questions spanning HTML, CSS, JavaScript and more. race against the clock while answering challenging questions, and aim for the highest score!
+
+## Features
+
+- 3 randomly selected questions for each playthrough.
+- Each question will give 4 possible answers for the user to click on.
+- 60 second countdown timer that starts when the user begins the quiz.
+- Wrong answers give a time penalty, decreasing the time by 10 seconds.
+- The quiz ends once the timer hits zero or all questions have been answered.
+- Score is calculated by total time left over.
+- once the user finishes the quiz they can submit their initials to record their score, reset the score board, and click the "go back to quiz" button to start again
+- High scores can be accessed before the quiz starts and after it ends.
+
+## How to Play
+
+- start the quiz by clicking on the "Start Quiz" button.
+- Navigate through questions by clicking the corresponding answer buttons. wrong choices will trigger a 10 second time penalty.
+- once complete, type in your initials and click "Save Score".
+- click "reset score" to reset the highscores board, or click "go back to quiz" to go back to the main menu
+- Click on the "Start Quiz" button to start a new fresh attempt
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//these are for getting information from the html into js
 var HeaderElement = document.getElementById('Header');
 var titleElement = document.getElementById('title');
 var startButton = document.getElementById('start-button');
@@ -14,31 +15,31 @@ var highScoresList = document.getElementById('high-scores-list');
 var resetHighScoresButton = document.getElementById('reset-high-scores-button');
 var backToQuizButton = document.getElementById('back-to-quiz-button');
 
+//this array stores all possible questions for the quiz, it is only used to store that information
 var originalQuestions = [
     { question: 'What does HTML stand for?', correctAnswer: 1, answers: ['Hot Tomato Markup Language', 'Hyper Text Markup Language', 'Home Tool Markup Language', 'Hyperlinks and Text Markup Language'] },
     { question: 'What does CSS stand for?', correctAnswer: 2, answers: ['Computer Style Sheet', 'Colorful Style Sheet', 'Cascading Style Sheet', 'Creative Style Sheet'] },
-    { question: 'JavaScript is a...', correctAnswer: 3, answers: ['Markup Language', 'Style Sheet Language', 'Database Management Language', 'Programming Language'] },
+    { question: 'JavaScript is a _____', correctAnswer: 3, answers: ['Markup Language', 'Style Sheet Language', 'Database Management Language', 'Programming Language'] },
     { question: 'What does API stand for?', correctAnswer: 0, answers: ['Application Programming Interface', 'Applied Programming Input', 'Application Performance Index', 'Advanced Pixel Integration'] },
     { question: 'Which of these is not a relational database?', correctAnswer: 1, answers: ['MySQL', 'MongoDB', 'PostgreSQL', 'Oracle'] },
     { question: 'What is a primary key in a database?', correctAnswer: 2, answers: ['A special key to unlock database', 'The most important data in the database', 'A unique identifier for records in the database', 'The first key in any database'] },
     { question: 'What does JSON stand for?', correctAnswer: 1, answers: ['Java Standard Object Notation', 'JavaScript Object Notation', 'Just a Simple Object Notation', 'JavaScript Oriented Notation'] },
     { question: 'Which of these is not a HTTP method?', correctAnswer: 3, answers: ['GET', 'POST', 'PUT', 'MAKE'] },
     { question: 'What does URL stand for?', correctAnswer: 2, answers: ['Unlimited Resources Locator', 'Universal Retrieval Link', 'Uniform Resource Locator', 'Unified Reference Link'] },
-    { question: 'In CSS, "px" is a unit for...', correctAnswer: 0, answers: ['Pixels', 'Percentage', 'Picas', 'Points'] },
+    { question: 'In CSS, "px" is a unit for ____', correctAnswer: 0, answers: ['Pixels', 'Percentage', 'Picas', 'Points'] },
     { question: 'Which HTML tag is used to define an internal style sheet?', correctAnswer: 1, answers: ['<style>', '<css>', '<script>', '<frame>'] },
     { question: 'Which HTML attribute is used to specify an alternate text for an image, if the image cannot be displayed?', correctAnswer: 0, answers: ['alt', 'src', 'href', 'title'] }
 ];
+
+//this array will be the one displayed on the application, since its a duplicate of originalQuestions we can manipulate it without losing questions
 var questions = [...originalQuestions];
 var currentQuestionIndex = 0;
+//this sets the timer to 60 seconds
 var timeLeft = 60;
 var timerInterval;
 var highScores = [];
 
-startButton.addEventListener('click', startQuiz);
-saveScoreButton.addEventListener('click', saveHighScore);
-resetHighScoresButton.addEventListener('click', resetHighScores);
-backToQuizButton.addEventListener('click', backToQuiz);
-
+// The startQuiz function will start the quiz and the timer, it also hides all unnecessary page information
 function startQuiz() {
     startButton.style.display = 'none';
     highScoresButton.style.display = 'none';
@@ -49,6 +50,7 @@ function startQuiz() {
     timerElement.innerText = "Time: " + timeLeft;
     questions = getRandomQuestions([...originalQuestions]);
 
+    //timer function
     timerInterval = setInterval(function() {
         timeLeft--;
         timerElement.innerText = "Time: " + timeLeft;
@@ -58,11 +60,13 @@ function startQuiz() {
     setNextQuestion();
 }
 
+// this function resets the state and displays the next question
 function setNextQuestion() {
     resetState();
     showQuestion(questions[currentQuestionIndex]);
 }
 
+// This function will return an array of random questions from the provided array
 function getRandomQuestions(questions) {
     let randomQuestions = [];
 
@@ -75,6 +79,7 @@ function getRandomQuestions(questions) {
     return randomQuestions;
 }
 
+//
 function showQuestion(question) {
     questionElement.innerText = question.question;
     question.answers.forEach((answer, index) => {
@@ -87,12 +92,14 @@ function showQuestion(question) {
     });
 }
 
+//function that resets the answer buttons so more can be loaded, uses a while loop to achieve this
 function resetState() {
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
 }
 
+//this function checks the selected answer and detracts time if an incorrect answer is selected
 function selectAnswer(e) {
     var selectedButton = e.target;
     var correct = selectedButton.dataset.correct;
@@ -107,12 +114,14 @@ function selectAnswer(e) {
     }
 }
 
+// this function resets the time and displays the game over screen
 function endQuiz() {
     clearInterval(timerInterval);
     quizContainer.style.display = 'none';
     gameOverContainer.style.display = 'block';
 }
 
+// this function creates a new object with users inputted initials and score and stores it into the highscores array
 function saveHighScore() {
     var initials = initialsInput.value;
         if (initialsInput.value === '') {
@@ -125,24 +134,27 @@ function saveHighScore() {
     showHighScores();
 }
 
+// this function displays the highScores page by removing un-needed elements and creates a high scores list
 function showHighScores() {
     highScoresContainer.style.display = 'block';
     highScoresButton.style.display = 'none'
     startButton.style.display = 'none';
     titleElement.style.display = 'none';
     highScoresList.innerHTML = '';
-    highScores.forEach(score => {
+    highScores.forEach(player => {
         var li = document.createElement('li');
-        li.innerText = `${score.initials}: ${score.score}`;
+        li.innerText = `${player.initials}: ${player.score}`;
         highScoresList.appendChild(li);
     });
 }
 
+//this function resets the highScores array
 function resetHighScores() {
     highScores = [];
     highScoresList.innerHTML = '';
 }
 
+// this function resets the timer and questions, while bringing back the main page elements
 function backToQuiz() {
     highScoresContainer.style.display = 'none';
     startButton.style.display = 'block';
